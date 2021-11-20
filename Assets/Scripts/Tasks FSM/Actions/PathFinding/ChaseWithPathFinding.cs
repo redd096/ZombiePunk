f0170827c250030e098b36aca54e129bb97137b9
redd096 <martin.babino96@gmail.com>
2021-11-20 01:10:19 +0100

    migliorato chase

--- Assets/Scripts/Tasks FSM/Actions/PathFinding/ChaseWithPathFinding.cs
+++ Assets/Scripts/Tasks FSM/Actions/PathFinding/ChaseWithPathFinding.cs
@@ -13,6 +13,15 @@ public class ChaseWithPathFinding : ActionTask<MovementComponent>
     [BlackboardOnly] public BBParameter<List<redd096.Node>> savePathAs;
     public bool repeat;
 
+    redd096.Node lastWalkableNode;
+
+    protected override string OnInit()
+    {
+        //reset var
+        lastWalkableNode = null;
+        return base.OnInit();
+    }
+
     protected override void OnUpdate()
     {
         //do only if there is target
@@ -29,13 +38,31 @@ public class ChaseWithPathFinding : ActionTask<MovementComponent>
         //if there is path, move to next node. When reach node, remove from the list
         if (savePathAs.value != null && savePathAs.value.Count > 0)
         {
+            if (lastWalkableNode != null)
+                lastWalkableNode = null;
+
             agent.MoveInDirection((savePathAs.value[0].worldPosition - agent.transform.position).normalized, speedChase.value);
             CheckReachNode();
         }
         //if there is no path, move straight to target
         else
         {
-            agent.MoveInDirection((target.value.position - agent.transform.position).normalized, speedChase.value);
+            //save last walkable node
+            if (lastWalkableNode == null)
+                lastWalkableNode = GameManager.instance.pathFindingAStar.Grid.NodeFromWorldPosition(agent.transform.position);
+
+            //check if player is in neighbours nodes, move straight to him
+            redd096.Node targetNode = GameManager.instance.pathFindingAStar.Grid.NodeFromWorldPosition(target.value.position);
+            if (GameManager.instance.pathFindingAStar.Grid.GetNeighbours(lastWalkableNode).Contains(targetNode))
+            {
+                agent.MoveInDirection((target.value.position - agent.transform.position).normalized, speedChase.value);
+            }
+            //else back to last walkable node
+            else
+            {
+                agent.MoveInDirection((lastWalkableNode.worldPosition - agent.transform.position).normalized, speedChase.value);
+            }
+
         }
 
         //end action if necessary
