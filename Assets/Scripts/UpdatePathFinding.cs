cb929c559f33dc797b965c8dec148f6bcf93c3f3
redd096 <martin.babino96@gmail.com>
2021-12-05 12:48:24 +0100

    inserito aggiornamento path finding quando si pushano i prop

---
+++ Assets/Scripts/UpdatePathFinding.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using UnityEngine;
+using redd096;
+using NaughtyAttributes;
+
+public class UpdatePathFinding : MonoBehaviour
+{
+    enum EUpdateModes { Update, FixedUpdate, Coroutine, None }
+
+    [Header("Necessary Components - default get from this gameObject")]
+    [Tooltip("To update when is moving")] [SerializeField] MovementComponent component;
+    [Tooltip("To update when is dead")] [SerializeField] HealthComponent healthComponent;
+
+    [Header("Updates")]
+    [Tooltip("Update or FixedUpdate?")] [SerializeField] EUpdateModes updateMode = EUpdateModes.Coroutine;
+    [Tooltip("Delay between updates using Coroutine method")] [EnableIf("updateMode", EUpdateModes.Coroutine)] [SerializeField] float timeCoroutine = 0.1f;
+    [SerializeField] bool updateOnMoves = true;
+    [SerializeField] bool updateOnDeath = true;
+
+    //update mode
+    Coroutine updateCoroutine;
+
+    void OnEnable()
+    {
+        //get references
+        if (component == null) component = GetComponent<MovementComponent>();
+        if (healthComponent == null) healthComponent = GetComponent<HealthComponent>();
+
+        if (component == null) Debug.LogWarning("Missing MovementComponent on " + name);
+        if (healthComponent == null) Debug.LogWarning("Missing HealthComponent on " + name);
+
+        //add events
+        if (healthComponent)
+        {
+            healthComponent.onDie += OnDie;
+        }
+
+        //start coroutine
+        if (updateMode == EUpdateModes.Coroutine)
+            updateCoroutine = StartCoroutine(UpdateCoroutine());
+    }
+
+    void OnDisable()
+    {
+        //remove events
+        if (healthComponent)
+        {
+            healthComponent.onDie -= OnDie;
+        }
+
+        //be sure to stop coroutine
+        if (updateCoroutine != null)
+        {
+            StopCoroutine(updateCoroutine);
+            updateCoroutine = null;
+        }
+    }
+
+    void Update()
+    {
+        //do only if update mode is Update
+        if (updateMode == EUpdateModes.Update)
+        {
+            //update on moves
+            if(updateOnMoves && component && component.CurrentSpeed > 0)
+                UpdatePath();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //do only if update mode is FixedUpdate
+        if (updateMode == EUpdateModes.FixedUpdate)
+        {
+            //update on moves
+            if (updateOnMoves && component && component.CurrentSpeed > 0)
+                UpdatePath();
+        }
+    }
+
+    IEnumerator UpdateCoroutine()
+    {
+        //do only if update mode is Coroutine
+        while (updateMode == EUpdateModes.Coroutine)
+        {
+            //update on moves
+            if (updateOnMoves && component && component.CurrentSpeed > 0)
+                UpdatePath();
+
+            yield return new WaitForSeconds(timeCoroutine);
+        }
+    }
+
+    void OnDie(Redd096Main whoDied)
+    {
+        //update on death
+        if(updateOnDeath)
+            UpdatePath();
+    }
+
+    void UpdatePath()
+    {
+        //update path
+        if (GameManager.instance && GameManager.instance.pathFindingAStar)
+            GameManager.instance.pathFindingAStar.UpdateGrid();
+    }
+}
