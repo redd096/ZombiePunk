2ba7968ca70351fcae05aebd6d02419bdf2578e1
Perdons22 <davide.perdon22@gmail.com>
2021-12-18 11:56:56 +0100

    Another test

---
+++ Assets/Commons/Prefabs/Levels/ObjectiveTest/ZombieSpawnTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawnTest : MonoBehaviour
+{
+    public GameObject zombie;
+    public float startDealy, interval;
+
+    private Transform spawn1, spawn2, spawn3, spawn4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawn1 = GameObject.Find("spawn1").GetComponent<Transform>();
+        spawn2 = GameObject.Find("spawn2").GetComponent<Transform>();
+        spawn3 = GameObject.Find("spawn3").GetComponent<Transform>();
+        spawn4 = GameObject.Find("spawn4").GetComponent<Transform>();
+
+        InvokeRepeating("SpawnZombies", startDealy, interval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnZombies()
+    {
+        Instantiate(zombie, spawn1.transform);
+        Instantiate(zombie, spawn2.transform);
+        Instantiate(zombie, spawn3.transform);
+        Instantiate(zombie, spawn4.transform);
+    }
+}
