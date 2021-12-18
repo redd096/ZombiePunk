9de2c090182cecd2493de9092fcc4a48aaa7800b
redd096 <martin.babino96@gmail.com>
2021-12-18 15:34:06 +0100

    creati spawner

---
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+[DefaultExecutionOrder(-1)]
+public class SpawnManager : MonoBehaviour
+{
+    //structs
+    [System.Serializable] struct SpawnStruct { public Spawn[] Spawns; }
+
+    [SerializeField] bool restartWhenFinish = false;
+    [ReorderableList] [SerializeField] SpawnStruct[] spawnsList = default;
+
+    [Header("DEBUG")]
+    [ReadOnly] [SerializeField] List<Spawn> spawnsActive = new List<Spawn>();
+    [ShowNonSerializedField] int index = 0;
+
+    //events
+    public System.Action<Spawn[]> onActiveList { get; set; }        //called when active a list of spawns
+    public System.Action<Spawn[]> onFinishList { get; set; }        //called when finish a list of spawns
+    public System.Action onFinishEveryList { get; set; }            //called when finish all the lists
+    public System.Action onRestart { get; set; }                    //called when finish all the lists and restart from the first
+    public System.Action onFinishToSpawn { get; set; }              //called when finish all the lists and stops
+
+
+    void Awake()
+    {
+        //deactive every spawn
+        for(int i = 0; i < spawnsList.Length; i++)
+        {
+            foreach (Spawn spawn in spawnsList[i].Spawns)
+            {
+                if (spawn == null)
+                    continue;
+
+                spawn.gameObject.SetActive(false);
+            }
+        }
+
+        //and active first list
+        ActiveList();
+    }
+
+    void OnKilledEverySpawnedObject(Spawn spawn)
+    {
+        //remove from the list and unregister from events
+        if(spawnsActive.Contains(spawn))
+        {
+            spawnsActive.Remove(spawn);
+            spawn.onEveryObjectIsKilled -= OnKilledEverySpawnedObject;
+        }
+
+        //check if finish to spawn this list
+        CheckFinishList();
+    }
+
+    void CheckFinishList()
+    {
+        //if finished
+        if (spawnsActive.Count <= 0)
+        {
+            //deactive every spawn
+            FinishList();
+
+            //active next list of spawns
+            if (index + 1 < spawnsList.Length)
+            {
+                index++;
+                ActiveList();
+            }
+            //if there aren't others, finish to spawn or restart
+            else
+            {
+                FinishEveryList();
+            }
+        }
+    }
+
+    #region private API
+
+    void FinishList()
+    {
+        //call event on finish list
+        onFinishList?.Invoke(spawnsList[index].Spawns);
+
+        //deactive every spawn
+        foreach(Spawn spawn in spawnsList[index].Spawns)
+        {
+            if (spawn == null)
+                continue;
+
+            spawn.gameObject.SetActive(false);
+        }
+    }
+
+    void ActiveList()
+    {
+        //active every spawn and register to their events
+        foreach (Spawn spawn in spawnsList[index].Spawns)
+        {
+            if (spawn == null)
+                continue;
+
+            spawn.gameObject.SetActive(true);
+            spawn.onEveryObjectIsKilled += OnKilledEverySpawnedObject;
+
+            //add to list, to know when every spawn of this list has finished
+            spawnsActive.Add(spawn);
+        }
+
+        //call event on active list
+        onActiveList?.Invoke(spawnsList[index].Spawns);
+    }
+
+    void FinishEveryList()
+    {
+        //call event on finish every list
+        onFinishEveryList?.Invoke();
+
+        //restart from the first
+        if(restartWhenFinish)
+        {
+            onRestart?.Invoke();
+
+            index = 0;
+            ActiveList();
+        }
+        //or finish to spawn
+        else
+        {
+            onFinishToSpawn?.Invoke();
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using NaughtyAttributes;
+using redd096;
+
+public class Spawn : MonoBehaviour
+{
+    [SerializeField] float timeBeforeFirstSpawn = 1;
+    [SerializeField] float timeBetweenSpawns = 5;
+    [SerializeField] GameObject[] prefabsToSpawn = default;
+
+    [Header("DEBUG")]
+    [ReadOnly] [SerializeField] List<HealthComponent> spawnedAlives = new List<HealthComponent>();
+    [ShowNonSerializedField] int index = 0;
+
+    //events
+    public System.Action<GameObject> onSpawn { get; set; }
+    public System.Action<Spawn> onFinishSpawn { get; set; }
+    public System.Action<Spawn> onEveryObjectIsKilled { get; set; }
+
+    Coroutine spawnCoroutine;
+
+    void OnEnable()
+    {
+        //start coroutine
+        spawnCoroutine = StartCoroutine(SpawnCoroutine());
+    }
+
+    void OnDisable()
+    {
+        //be sure to stop coroutine
+        if (spawnCoroutine != null)
+            StopCoroutine(spawnCoroutine);
+    }
+
+    IEnumerator SpawnCoroutine()
+    {
+        for(index = 0; index < prefabsToSpawn.Length; index++)
+        {
+            //wait
+            yield return new WaitForSeconds(index <= 0 ? timeBeforeFirstSpawn : timeBetweenSpawns);
+
+            //then spawn
+            SpawnObject();
+        }
+
+        //call event
+        onFinishSpawn?.Invoke(this);
+
+        spawnCoroutine = null;
+    }
+
+    void OnKilledObject(HealthComponent whoDied)
+    {
+        //remove from the list and unregister from event
+        if(spawnedAlives.Contains(whoDied))
+        {
+            spawnedAlives.Remove(whoDied);
+            whoDied.onDie -= OnKilledObject;
+        }
+        
+        //if has finished to spawn, check if every spawned object was killed
+        if(spawnCoroutine == null)
+        {
+            if(spawnedAlives.Count <= 0)
+            {
+                //call event
+                onEveryObjectIsKilled?.Invoke(this);
+            }
+        }
+    }
+
+    #region private API
+
+    void SpawnObject()
+    {
+        if (prefabsToSpawn[index])
+        {
+            //spawn
+            GameObject go = Instantiate(prefabsToSpawn[index], transform.position, transform.rotation);
+            go.transform.SetParent(transform);
+
+            //if has health, register to events and add to list
+            HealthComponent health = go.GetComponent<HealthComponent>();
+            if (health)
+            {
+                health.onDie += OnKilledObject;
+                spawnedAlives.Add(health);
+            }
+
+            //call event
+            onSpawn?.Invoke(go);
+        }
+    }
+
+    #endregion
+}
