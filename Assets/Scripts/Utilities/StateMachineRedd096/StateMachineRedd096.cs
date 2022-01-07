37cb34bfec3eb6633cc257a1a0ceef94952a8aa4
redd096 <martin.babino96@gmail.com>
2022-01-07 17:35:57 +0100

    inserito evento state machine

--- Assets/Scripts/Utilities/StateMachineRedd096/StateMachineRedd096.cs
+++ Assets/Scripts/Utilities/StateMachineRedd096/StateMachineRedd096.cs
@@ -37,6 +37,9 @@ namespace redd096
         //blackboard to save vars to use in differents tasks
         Dictionary<string, object> blackboard = new Dictionary<string, object>();
 
+        //events
+        public System.Action<string> onSetState { get; set; }
+
         void Start()
         {
             //start with first state
@@ -75,6 +78,9 @@ namespace redd096
             {
                 EnterState();
             }
+
+            //call event
+            onSetState?.Invoke(CurrentState != null ? CurrentState.StateName : "");
         }
 
         #region private API
