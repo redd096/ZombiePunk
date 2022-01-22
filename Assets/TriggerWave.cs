72f96d3cebf4d09e9f51fc995a819064e55213ad
daX-_-_908 <dam992011@gmail.com>
2022-01-22 10:32:32 +0100

    Apparently working trigger

---
+++ Assets/TriggerWave.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerWave : MonoBehaviour
+{
+    public GameObject Player;
+
+    public GameObject Spawns;
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Spawns.SetActive(true);
+
+    }
+
+
+}
