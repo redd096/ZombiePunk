5f498c1ad01b88b8762fcff08ce2528d4abac79f
Perdons22 <davide.perdon22@gmail.com>
2022-01-22 16:34:03 +0100

    Semi-working Destruction Objective

---
+++ Assets/Commons/Prefabs/Levels/ObjectiveShit/ToDestroyS.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using redd096;
+
+public class ToDestroyS : MonoBehaviour
+{
+    private HealthComponent health;
+    private DoorController door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = GetComponent<HealthComponent>();
+        print("Ok");
+        door = GameObject.Find("Door").GetComponent<DoorController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health.CurrentHealth <= 0)
+        {
+            print("BruttaMerda");
+            door.objectsNumber -= 1;
+        }
+    }
+}
