c5b8caa980bc63fb8f6a6b2c3bc57a908fd8b8eb
daX-_-_908 <dam992011@gmail.com>
2022-01-22 11:10:13 +0100

    Trigger working even better than before

---
+++ Assets/StartTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartTrigger : MonoBehaviour
+{
+    public GameObject Player;
+
+    public GameObject Spawns;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Spawns.SetActive(true);
+    }
+
+}
--- Assets/TriggerWave.cs
+++ Assets/TriggerWave.cs
@@ -7,12 +7,24 @@ public class TriggerWave : MonoBehaviour
     public GameObject Player;
 
     public GameObject Spawns;
+
+    public GameObject Deactive;
+
+    public GameObject Block;
     
     void OnTriggerEnter2D(Collider2D other)
     {
         Spawns.SetActive(true);
 
+        Deactive.SetActive(false);
+
     }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Block.SetActive(true);
+    }
+
 
 
 }
