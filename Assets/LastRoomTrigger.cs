5f0a9186d2491f42d2336e0c51cf34568e2db8c0
daX-_-_908 <dam992011@gmail.com>
2022-01-23 23:50:09 +0100

    Fix Exit

---
+++ Assets/LastRoomTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LastRoomTrigger : MonoBehaviour
+{
+    public GameObject Player;
+
+    public GameObject Spawns;
+
+    public GameObject Deactive;
+
+    public GameObject Block;
+
+    public GameObject Exit;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Spawns.SetActive(true);
+
+        Deactive.SetActive(false);
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Block.SetActive(true);
+
+        Exit.SetActive(true);
+    }
+
+}
