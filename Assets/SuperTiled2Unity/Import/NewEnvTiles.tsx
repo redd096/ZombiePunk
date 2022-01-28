d68b2a5dc0e3df9f835cb6d967a4af9d8eefa879
Perdons22 <davide.perdon22@gmail.com>
2022-01-28 15:44:13 +0100

    Tiled Test

---
+++ Assets/SuperTiled2Unity/Import/NewEnvTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="NewEnvTiles" tilewidth="16" tileheight="16" tilecount="364" columns="26">
+ <image source="istruzioni_tileset.png" width="416" height="224"/>
+</tileset>
