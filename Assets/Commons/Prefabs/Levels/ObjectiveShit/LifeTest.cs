3662ec4c6a4647bec1978c1b97f4fbd77b1c1c96
Perdons22 <davide.perdon22@gmail.com>
2022-02-11 16:42:54 +0100

    UI life test

---
+++ Assets/Commons/Prefabs/Levels/ObjectiveShit/LifeTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using redd096;
+using UnityEngine.UI;
+
+public class LifeTest : MonoBehaviour
+{
+    public HealthComponent life;
+    private Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = gameObject.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        image.fillAmount = (life.CurrentHealth) / 100;
+    }
+}
