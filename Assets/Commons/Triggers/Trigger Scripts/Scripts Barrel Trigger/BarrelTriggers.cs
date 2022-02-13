62170a18805409adca4e79a8bb16de9b24c0beae
daX-_-_908 <dam992011@gmail.com>
2022-02-13 10:04:37 +0100

    Added patterns to the zombie shooter

---
+++ Assets/Commons/Triggers/Trigger Scripts/Scripts Barrel Trigger/BarrelTriggers.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelTriggers : MonoBehaviour
+{
+    public GameObject BarrelsOn;
+    public GameObject BarrelsOff;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        BarrelsOn.SetActive(true);
+        BarrelsOff.SetActive(false);
+    }
+}
