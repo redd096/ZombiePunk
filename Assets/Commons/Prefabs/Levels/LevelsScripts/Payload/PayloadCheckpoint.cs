e7733d5d7884a67c1095a2a02a504a5c6a37d207
Perdons22 <davide.perdon22@gmail.com>
2022-02-20 17:30:07 +0100

    Payload checkpoint

---
+++ Assets/Commons/Prefabs/Levels/LevelsScripts/Payload/PayloadCheckpoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PayloadCheckpoint : MonoBehaviour
+{
+    public GameObject toTurnOn1,toTurnOn2, toTurnOff;
+
+    private GameObject payload;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        payload = GameObject.Find("Payload");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.x <= payload.transform.position.x)
+        {
+            toTurnOn1.SetActive(true);
+            toTurnOn2.SetActive(true);
+            Destroy(toTurnOff);
+        }
+    }
+}
