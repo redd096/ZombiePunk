c712466a8bcc4e2331a2e69aa665bd74555cd1bd
daX-_-_908 <dam992011@gmail.com>
2022-02-21 18:04:30 +0100

    Jumper Level

---
+++ Assets/Commons/Triggers/Trigger Scripts/Scripts Spawners/LastRoomTriggerWith2Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LastRoomTriggerWith2Spawner : MonoBehaviour
+{
+    public GameObject Spawn1, Spawn2;
+
+    public GameObject Deactive;
+
+    public GameObject Block;
+
+    public GameObject Exit;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Spawn1.SetActive(true);
+        Spawn2.SetActive(true);
+
+        Destroy(Deactive);
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Block.SetActive(true);
+
+        Exit.SetActive(true);
+    }
+
+}
