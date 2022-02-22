e024b8649da406ff013e4ad75892546858c8412d
redd096 <martin.babino96@gmail.com>
2022-02-22 09:54:45 +0100

    spawn weapon prefab + sprite mostrata in scena

--- Assets/Scripts/Weapons/SpawnWeapon.cs
+++ Assets/Scripts/Weapons/SpawnWeapon.cs
@@ -5,8 +5,29 @@ public class SpawnWeapon : MonoBehaviour
 {
     [SerializeField] WeaponBASE weaponPrefab = default;
 
+    //editor
+    [HideInInspector] [SerializeField] SpriteRenderer spriteInEditor = default;
+
+    void OnValidate()
+    {
+        //only in editor
+        if (Application.isPlaying == false)
+        {
+            //add sprite renderer
+            if (spriteInEditor == null)
+                spriteInEditor = gameObject.AddComponent<SpriteRenderer>();
+
+            //set sprite weapon
+            spriteInEditor.sprite = weaponPrefab ? weaponPrefab.WeaponSprite : null;
+        }
+    }
+
     void Awake()
     {
+        //remove sprite renderer in game
+        if (spriteInEditor)
+            Destroy(spriteInEditor);
+
         if (weaponPrefab)
         {
             //instantiate weapon and save prefab
