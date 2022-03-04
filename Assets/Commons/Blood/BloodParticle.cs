69b85a98e6f6f201fb2ba4b52b6aba56e1faf96b
daX-_-_908 <dam992011@gmail.com>
2022-03-04 08:16:31 +0100

    Test BloodParticle

---
+++ Assets/Commons/Blood/BloodParticle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodParticle : MonoBehaviour
+{
+    public GameObject Blood;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            Blood.GetComponent<ParticleSystem>().Play();
+        }
+    }
+}
