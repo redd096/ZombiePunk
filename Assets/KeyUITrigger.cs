ebd8075ee465357c168d8b10581d72a40f520a7c
daX-_-_908 <dam992011@gmail.com>
2022-03-14 22:51:08 +0100

    Added UI key "E" to interact with the exits

---
+++ Assets/KeyUITrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyUITrigger : MonoBehaviour
+{
+    public GameObject UI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            UI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            UI.SetActive(false);
+        }
+    }
+}
