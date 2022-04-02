d0f9bbe6d5ae44c2b6845befe6076bdf95bf690a
Perdons22 <davide.perdon22@gmail.com>
2022-04-02 16:42:19 +0200

    New Frogs

---
+++ Assets/Commons/Prefabs/Levels/LevelsScripts/EnemiesOnDisable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesOnDisable : MonoBehaviour
+{
+    public GameObject toActivate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnDisable()
+    {
+        toActivate.SetActive(true);
+    }
+}
