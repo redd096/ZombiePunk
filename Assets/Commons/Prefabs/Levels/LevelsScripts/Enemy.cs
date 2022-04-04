52f14509170ba48f0cb531c641ee6332a4b1d4df
Perdons22 <davide.perdon22@gmail.com>
2022-04-04 17:03:13 +0200

    Fully operational Frogger

---
+++ Assets/Commons/Prefabs/Levels/LevelsScripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private KillAllEnemies stuff;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        stuff = GameObject.FindObjectOfType<KillAllEnemies>();
+        stuff.AddEnemy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        stuff.RemoveEnemy();
+    }
+}
---
+++ Assets/Commons/Prefabs/Levels/LevelsScripts/KillAllEnemies.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class KillAllEnemies : MonoBehaviour
+    {
+        public GameObject play, exit;
+
+        private int open;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+            Debug.Log(open);
+
+            if(open > 0)
+            {
+                play.SetActive(true);
+                exit.SetActive(false);
+            }
+            else
+            {
+                play.SetActive(false);
+                exit.SetActive(true);
+            }
+        }
+
+        public void AddEnemy()
+        {
+            open += 1;
+        }
+
+        public void RemoveEnemy()
+        {
+            open -= 1;
+        }
+    }
