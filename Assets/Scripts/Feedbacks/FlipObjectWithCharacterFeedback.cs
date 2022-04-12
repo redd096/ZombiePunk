58ccda3c414d6fdb8600187fc9952b50965c22bf
redd096 <martin.babino96@gmail.com>
2022-04-12 12:20:58 +0200

    inserito FlipObjectWithCharacterFeedback per ruotare gli oggetti quando si flippa il character

---
+++ Assets/Scripts/Feedbacks/FlipObjectWithCharacterFeedback.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+
+namespace redd096.GameTopDown2D
+{
+    [AddComponentMenu("redd096/.GameTopDown2D/Feedbacks/Flip Object With Character Feedback")]
+    public class FlipObjectWithCharacterFeedback : MonoBehaviour
+    {
+        enum EFlipType { BasedOnAim, BasedOnMovement }
+
+        [Header("Necessary Components - default get in parent")]
+        [SerializeField] MovementComponent movementComponent = default;
+        [SerializeField] AimComponent aimComponent = default;
+
+        [Header("object to flip (already in scene/prefab)")]
+        [SerializeField] GameObject objectToFlip = default;
+
+        [Header("Flip Rules")]
+        [SerializeField] bool defaultAtLeftOfCharacter = true;
+        [SerializeField] EFlipType flipType = EFlipType.BasedOnAim;
+        [SerializeField] bool flipAlsoScale = false;
+
+        bool ownerIsLookingRight;
+
+        void Awake()
+        {
+            //set default value
+            ownerIsLookingRight = defaultAtLeftOfCharacter;
+        }
+
+        void OnEnable()
+        {
+            if (movementComponent == null) movementComponent = GetComponentInParent<MovementComponent>();
+            if (aimComponent == null) aimComponent = GetComponentInParent<AimComponent>();
+
+            //add events
+            if (movementComponent)
+            {
+                movementComponent.onChangeMovementDirection += OnChangeMovementDirection;
+                OnChangeMovementDirection(movementComponent.IsMovingRight); //set default value
+            }
+            if (aimComponent)
+            {
+                aimComponent.onChangeAimDirection += OnChangeAimDirection;
+                OnChangeAimDirection(aimComponent.IsLookingRight);          //set default value
+            }
+        }
+
+        void OnDisable()
+        {
+            //remove events
+            if (movementComponent)
+            {
+                movementComponent.onChangeMovementDirection -= OnChangeMovementDirection;
+            }
+            if (aimComponent)
+            {
+                aimComponent.onChangeAimDirection -= OnChangeAimDirection;
+            }
+        }
+
+        #region events
+
+        void OnChangeMovementDirection(bool movingRight)
+        {
+            //rotate based on movement
+            if (objectToFlip && flipType == EFlipType.BasedOnMovement)
+            {
+                if (movingRight != ownerIsLookingRight)
+                {
+                    ownerIsLookingRight = movingRight;
+
+                    //rotate scale and change offset
+                    objectToFlip.transform.localPosition = new Vector2(-objectToFlip.transform.localPosition.x, objectToFlip.transform.localPosition.y);
+                    if (flipAlsoScale) objectToFlip.transform.localScale = new Vector2(-objectToFlip.transform.localScale.x, objectToFlip.transform.localScale.y);
+                }
+            }
+        }
+
+        void OnChangeAimDirection(bool lookingRight)
+        {
+            //rotate based on aim
+            if (objectToFlip && flipType == EFlipType.BasedOnAim)
+            {
+                if (lookingRight != ownerIsLookingRight)
+                {
+                    ownerIsLookingRight = lookingRight;
+
+                    //rotate scale and change offset
+                    objectToFlip.transform.localPosition = new Vector2(-objectToFlip.transform.localPosition.x, objectToFlip.transform.localPosition.y);
+                    if (flipAlsoScale) objectToFlip.transform.localScale = new Vector2(-objectToFlip.transform.localScale.x, objectToFlip.transform.localScale.y);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
--- Assets/Scripts/Feedbacks/StateMachineFeedback.cs
+++ Assets/Scripts/Feedbacks/StateMachineFeedback.cs
@@ -1,63 +1,64 @@
 ﻿using UnityEngine;
-using redd096;
-using redd096.GameTopDown2D;
 
-[System.Serializable]
-public struct AnimTriggerStateStruct
+namespace redd096.GameTopDown2D
 {
-    public string State;
-    public string TriggerToSet;
-}
-
-[AddComponentMenu("redd096/Feedbacks/StateMachine Feedback")]
-public class StateMachineFeedback : MonoBehaviour
-{
-    [Header("Necessary Components - default get in child and parent")]
-    [SerializeField] Animator anim = default;
-    [SerializeField] StateMachineRedd096 stateMachine = default;
-
-    [Header("States and triggers in animator")]
-    [SerializeField] AnimTriggerStateStruct[] animTriggers = default;
-
-    void OnEnable()
+    [AddComponentMenu("redd096/.GameTopDown2D/Feedbacks/StateMachine Feedback")]
+    public class StateMachineFeedback : MonoBehaviour
     {
-        //get references
-        if (anim == null) anim = GetComponentInChildren<Animator>();
-        if (stateMachine == null)
-        {
-            Redd096Main main = GetComponentInParent<Redd096Main>();
-            if (main) stateMachine = main.GetComponentInChildren<StateMachineRedd096>();
-        }
+        [Header("Necessary Components - default get in child and parent")]
+        [SerializeField] Animator anim = default;
+        [SerializeField] StateMachineRedd096 stateMachine = default;
 
-        //add events
-        if (stateMachine)
-        {
-            stateMachine.onSetState += OnSetState;
-        }
-    }
+        [Header("States and triggers in animator")]
+        [SerializeField] AnimTriggerStateStruct[] animTriggers = default;
 
-    void OnDisable()
-    {
-        //remove events
-        if (stateMachine)
+        void OnEnable()
         {
-            stateMachine.onSetState -= OnSetState;
-        }
-    }
-
-    void OnSetState(string stateName)
-    {
-        foreach (AnimTriggerStateStruct animTrigger in animTriggers)
-        {
-            //if enter in this state
-            if (stateName.Equals(animTrigger.State))
+            //get references
+            if (anim == null) anim = GetComponentInChildren<Animator>();
+            if (stateMachine == null)
             {
-                //set trigger
-                if (anim)
-                    anim.SetTrigger(animTrigger.TriggerToSet);
+                Redd096Main main = GetComponentInParent<Redd096Main>();
+                if (main) stateMachine = main.GetComponentInChildren<StateMachineRedd096>();
+            }
 
-                break;
+            //add events
+            if (stateMachine)
+            {
+                stateMachine.onSetState += OnSetState;
             }
         }
+
+        void OnDisable()
+        {
+            //remove events
+            if (stateMachine)
+            {
+                stateMachine.onSetState -= OnSetState;
+            }
+        }
+
+        void OnSetState(string stateName)
+        {
+            foreach (AnimTriggerStateStruct animTrigger in animTriggers)
+            {
+                //if enter in this state
+                if (stateName.Equals(animTrigger.State))
+                {
+                    //set trigger
+                    if (anim)
+                        anim.SetTrigger(animTrigger.TriggerToSet);
+
+                    break;
+                }
+            }
+        }
+    }
+
+    [System.Serializable]
+    public struct AnimTriggerStateStruct
+    {
+        public string State;
+        public string TriggerToSet;
     }
 }
