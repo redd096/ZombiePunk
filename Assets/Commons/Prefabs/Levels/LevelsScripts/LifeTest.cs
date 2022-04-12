8df7a44d3703a5de6d8289e6fb09fe499714387f
redd096 <martin.babino96@gmail.com>
2022-04-12 11:00:47 +0200

    inserito predict

--- Assets/Commons/Prefabs/Levels/LevelsScripts/LifeTest.cs
+++ Assets/Commons/Prefabs/Levels/LevelsScripts/LifeTest.cs
@@ -3,22 +3,99 @@ using System.Collections.Generic;
 using UnityEngine;
 using redd096.GameTopDown2D;
 using UnityEngine.UI;
+using redd096.Attributes;
 
 public class LifeTest : MonoBehaviour
 {
-    private HealthComponent life;
-    private Image image;
+    [Header("Necessary Components")]
+    [SerializeField] HealthComponent life = default;
+    [SerializeField] Image image = default;
 
-    // Start is called before the first frame update
-    void Start()
+    [Header("Predict")]
+    [SerializeField] bool enablePredict = true;
+    [EnableIf("enablePredict")] [SerializeField] Image predictionImage = default;
+    [EnableIf("enablePredict")] [Tooltip("For how much time prediction will remain still")] [SerializeField] float durationPrediction = 0.5f;
+    [EnableIf("enablePredict")] [Tooltip("Duration animation from start value to end value")] [SerializeField] float timeToReachValue = 0.5f;
+    [EnableIf("enablePredict")] [SerializeField] Color colorOnIncrease = Color.green;
+    [EnableIf("enablePredict")] [SerializeField] Color colorOnDecrease = Color.red;
+
+    Coroutine updateHealthBarCoroutine;
+
+    void OnEnable()
     {
-        image = gameObject.GetComponent<Image>();
-        life = GameObject.Find("Player").GetComponent<HealthComponent>();
+        //find components
+        if (image == null) image = gameObject.GetComponent<Image>();
+        if (life == null) life = GameObject.Find("Player").GetComponent<HealthComponent>();
+
+        //add events
+        if (life)
+        {
+            life.onChangeHealth += UpdateLife;
+            UpdateLife(); //set default value
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnDisable()
     {
-        image.fillAmount = (life.CurrentHealth) / 100;
+        //remove events
+        if (life)
+        {
+            life.onChangeHealth -= UpdateLife;
+        }
+    }
+
+    void UpdateLife()
+    {
+        //do nothing if image is not setted
+        if (image == null)
+            return;
+
+        //if predict is not enabled or there isn't prediction image, just set health value
+        if (enablePredict == false || predictionImage == null)
+        {
+            image.fillAmount = life.CurrentHealth / life.MaxHealth;
+        }
+        //else start coroutine to update health using prediction too
+        else
+        {
+            if (updateHealthBarCoroutine != null)
+                StopCoroutine(updateHealthBarCoroutine);
+
+            updateHealthBarCoroutine = StartCoroutine(UpdateHealthBarCoroutine());
+        }
+    }
+
+    IEnumerator UpdateHealthBarCoroutine()
+    {
+        //check if increase or decrease
+        float valueToReach = life.CurrentHealth / life.MaxHealth;
+        bool increase = valueToReach >= predictionImage.fillAmount;
+
+        //set prediction color (increase or decrease)
+        predictionImage.color = increase ? colorOnIncrease : colorOnDecrease;
+
+        //if increase, move prediction first, else move healthBar
+        if (increase)
+            predictionImage.fillAmount = valueToReach;
+        else
+            image.fillAmount = valueToReach;
+
+        //wait duration
+        yield return new WaitForSeconds(durationPrediction);
+
+        //then start animation
+        float startValue = increase ? image.fillAmount : predictionImage.fillAmount;
+        float delta = 0;
+        while (delta < 1)
+        {
+            delta += Time.deltaTime / timeToReachValue;
+
+            if (increase)
+                image.fillAmount = Mathf.Lerp(startValue, valueToReach, delta);
+            else
+                predictionImage.fillAmount = Mathf.Lerp(startValue, valueToReach, delta);
+
+            yield return null;
+        }
     }
 }
