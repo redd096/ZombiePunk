f61efb0b5beb1d7d0f8647653cd1486c4e90e9be
daX-_-_908 <dam992011@gmail.com>
2022-04-15 18:45:31 +0200

    Fixes

---
+++ Assets/Commons/Sprites/UI/Scripts UI/ObjectiveCompleted.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ObjectiveCompleted : MonoBehaviour
+{
+    //public Animation completedanim;
+    public GameObject Test;
+
+    private redd096.GameTopDown2D.ExitInteractable exit; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        exit = GetComponent<redd096.GameTopDown2D.ExitInteractable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FinishedObjective()
+    {
+        if (exit.IsOpen)
+        {
+            Test.SetActive(true);
+        }
+    }
+}
