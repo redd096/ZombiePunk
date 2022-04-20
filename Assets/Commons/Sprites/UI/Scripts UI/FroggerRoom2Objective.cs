fb0ec93b63fa8f41314d24727261ffa47eb7b94f
daX-_-_908 <dam992011@gmail.com>
2022-04-20 18:23:07 +0200

    Added shells to all the guns and other changes

--- Assets/Commons/Sprites/UI/Scripts UI/FroggerRoom2Objective.cs
+++ Assets/Commons/Sprites/UI/Scripts UI/FroggerRoom2Objective.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class FroggerRoom2Objective : MonoBehaviour
 {
+    public TMP_Text billboardsnumber;
+    private int upnum;
+    private FroggerDoor counter;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        counter = GetComponent<FroggerDoor>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        counting();
     }
+    void counting()
+    {
+        if (counter.number <= 4)
+        {
+            upnum++;
+            billboardsnumber.text = upnum.ToString();
+        }
+    }
+
 }
