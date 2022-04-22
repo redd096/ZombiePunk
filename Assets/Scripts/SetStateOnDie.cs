fc62b6a4ae43db1260f8c1bfa595793e3e57f127
redd096 <martin.babino96@gmail.com>
2022-04-22 17:56:50 +0200

    ora gli oggetti non vengono distrutti istant quando muoiono

---
+++ Assets/Scripts/SetStateOnDie.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using redd096.GameTopDown2D;
+using redd096;
+
+public class SetStateOnDie : MonoBehaviour
+{
+    HealthComponent healthComponent;
+    StateMachineRedd096 stateMachine;
+
+    void OnEnable()
+    {
+        //get references
+        healthComponent = GetComponentInParent<HealthComponent>();
+        stateMachine = GetComponentInChildren<StateMachineRedd096>();
+
+        //add events
+        if (healthComponent)
+        {
+            healthComponent.onDie += OnDie;
+        }
+    }
+
+    void OnDisable()
+    {
+        //remove events
+        if (healthComponent)
+        {
+            healthComponent.onDie -= OnDie;
+        }
+    }
+
+    private void OnDie(HealthComponent whoDied)
+    {
+        //set state null, when die
+        if (stateMachine)
+            stateMachine.SetState(-1);
+    }
+}
