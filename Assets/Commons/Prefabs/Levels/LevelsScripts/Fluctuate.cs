dd7be4c7b9a1a5f6b8454bcf1e6254ed4de9b653
redd096 <martin.babino96@gmail.com>
2022-04-26 11:20:51 +0200

    coin fluctuate

---
+++ Assets/Commons/Prefabs/Levels/LevelsScripts/Fluctuate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Fluctuate : MonoBehaviour
+{
+    [SerializeField] float height = 0.2f;
+
+    Vector2 startPosition;
+
+    void Start()
+    {
+        //save start position
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = startPosition + Vector2.up * Mathf.Sin(Time.time) * height;
+    }
+}
