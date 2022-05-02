a5feb5336c1ef2ddc79d7f487bd0a963e7a9d643
redd096 <martin.babino96@gmail.com>
2022-05-02 19:13:44 +0200

    ora si possono inserire degli stati per quando stoppare l'attacco

--- Assets/Scripts/StopAttackOnDamage.cs
+++ Assets/Scripts/StopAttackOnDamage.cs
@@ -1,15 +1,29 @@
-﻿using System.Collections;
+﻿using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 using redd096.GameTopDown2D;
 using redd096;
 
 public class StopAttackOnDamage : MonoBehaviour
 {
+    public enum ETypeOfCheck { IgnoreInTheseStates, CheckOnlyInTheseStates }
+
     [SerializeField] float timeBeforeResetState = 0.2f;
+
+    [Header("Ignore when in these states, or check only in these states")]
+    [SerializeField] ETypeOfCheck typeOfCheck = ETypeOfCheck.IgnoreInTheseStates;
+    [SerializeField] List<string> states = default;
 
     HealthComponent healthComponent;
     StateMachineRedd096 stateMachine;
     Coroutine resetStateCoroutine;
+
+    void Awake()
+    {
+        //remove every white space and set upper case
+        for (int i = 0; i < states.Count; i++)
+            states[i] = states[i].Replace(" ", "").ToUpper();
+    }
 
     void OnEnable()
     {
@@ -45,6 +59,19 @@ public class StopAttackOnDamage : MonoBehaviour
         //set state null, when get damage
         if (stateMachine)
         {
+            if (states.Contains(stateMachine.CurrentState.StateName.Replace(" ", "").ToUpper()))
+            {
+                //if ignore in these states, and current state is in the list, return
+                if (typeOfCheck == ETypeOfCheck.IgnoreInTheseStates)
+                    return;
+            }
+            else
+            {
+                //if check only in these states, but current state is not in the list, return
+                if (typeOfCheck == ETypeOfCheck.CheckOnlyInTheseStates)
+                    return;
+            }
+
             stateMachine.SetState(-1);
 
             //start coroutine
