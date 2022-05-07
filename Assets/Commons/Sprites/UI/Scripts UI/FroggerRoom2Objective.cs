3739dc6aa2dd75596e3ad07a8b2eda0996a18c79
daX-_-_908 <dam992011@gmail.com>
2022-05-07 16:16:55 +0200

    Adjustments

--- Assets/Commons/Sprites/UI/Scripts UI/FroggerRoom2Objective.cs
+++ Assets/Commons/Sprites/UI/Scripts UI/FroggerRoom2Objective.cs
@@ -17,17 +17,17 @@ public class FroggerRoom2Objective : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        counting();
+   // void Update()
+    //{
+        //counting();
     }
    // void counting()
-    {
+  //  {
        // if (counter.number <= 4)
       //  {
         //    upnum++;
           //  billboardsnumber.text = upnum.ToString();
         //}
-    }
+   // }
 
-}
+//
