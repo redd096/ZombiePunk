26dcb7f64bf0cf299b1388ce705dd30e19cafc78
Perdons22 <davide.perdon22@gmail.com>
2022-05-16 19:46:04 +0200

    THOMAS THE TANK ENGINE IS BACK (sort of)

---
+++ Assets/Commons/Prefabs/Levels/LevelsScripts/ThomasBackground.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThomasBackground : MonoBehaviour
+{
+    public float speed;
+
+    private Vector3 startPos;
+    private float repeatWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider2D>().size.x / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+        if (transform.position.x < startPos.x - repeatWidth)
+        {
+            transform.position = startPos;
+        }
+    }
+}
