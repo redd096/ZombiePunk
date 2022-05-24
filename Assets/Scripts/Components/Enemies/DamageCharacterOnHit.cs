33ddb8d591b9fd15ab17c81b141c8fabc916b24c
redd096 <martin.babino96@gmail.com>
2022-05-24 11:03:36 +0200

    sono addormentato, ma ora dovreste poter scegliere se usare trigger o collision nel DamageCharacterOnHit (payload)

--- Assets/Scripts/Components/Enemies/DamageCharacterOnHit.cs
+++ Assets/Scripts/Components/Enemies/DamageCharacterOnHit.cs
@@ -6,8 +6,9 @@ namespace redd096.GameTopDown2D
     [AddComponentMenu("redd096/.GameTopDown2D/Components/Enemies/Damage Character On Hit")]
     public class DamageCharacterOnHit : MonoBehaviour
     {
-        [Header("Do Damage On Hit?")]
+        [Header("Do Damage On Hit? - Collision or Trigger?")]
         [SerializeField] bool isActive = true;
+        [SerializeField] bool useTrigger = false;
 
         [Header("Damage")]
         [SerializeField] List<Character.ECharacterType> charactersToHit = new List<Character.ECharacterType>() { Character.ECharacterType.Player };
@@ -31,32 +32,90 @@ namespace redd096.GameTopDown2D
             selfCharacter = GetComponent<Character>();
         }
 
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (useTrigger && CheckCollisionEnter(collision.transform))
+            {
+                //damage it and add to the list
+                OnHit(hitCharacter, collision.ClosestPoint(transform.position));
+                hits.Add(hitCharacter, Time.time + delayBetweenAttacks);        //set timer
+            }
+        }
+
+        void OnTriggerStay2D(Collider2D collision)
+        {
+            if (useTrigger && CheckCollisionEnter(collision.transform))
+            {
+                OnHit(hitCharacter, collision.ClosestPoint(transform.position));
+                hits[hitCharacter] = Time.time + delayBetweenAttacks;           //reset timer
+            }
+        }
+
+        void OnTriggerExit2D(Collider2D collision)
+        {
+            if (useTrigger && CheckCollisionEnter(collision.transform))
+            {
+                //remove from the list
+                hits.Remove(hitCharacter);
+            }
+        }
+
         void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (useTrigger == false && CheckCollisionEnter(collision.transform))
+            {
+                //damage it and add to the list
+                OnHit(hitCharacter, collision.GetContact(0).point);
+                hits.Add(hitCharacter, Time.time + delayBetweenAttacks);        //set timer
+            }
+        }
+
+        void OnCollisionStay2D(Collision2D collision)
+        {
+            if (useTrigger == false && CheckCollisionEnter(collision.transform))
+            {
+                OnHit(hitCharacter, collision.GetContact(0).point);
+                hits[hitCharacter] = Time.time + delayBetweenAttacks;           //reset timer
+            }
+        }
+
+        void OnCollisionExit2D(Collision2D collision)
+        {
+            if (useTrigger == false && CheckCollisionEnter(collision.transform))
+            {
+                //remove from the list
+                hits.Remove(hitCharacter);
+            }
+        }
+
+        #region private API
+
+        bool CheckCollisionEnter(Transform collision)
         {
             //do nothing if deactivated (use boolean because the unity toggle doesn't work with collision enter)
             if (isActive == false)
-                return;
+                return false;
 
             //check if hit character and is not already in the list
-            hitCharacter = collision.transform.GetComponentInParent<Character>();
+            hitCharacter = collision.GetComponentInParent<Character>();
             if (hitCharacter && hits.ContainsKey(hitCharacter) == false)
             {
                 //check can damage
                 if ((selfCharacter == null || hitCharacter != selfCharacter) &&     //be sure to not hit self
                     charactersToHit.Contains(hitCharacter.CharacterType))           //and be sure is type can hit
                 {
-                    //damage it and add to the list
-                    OnHit(collision, hitCharacter);
-                    hits.Add(hitCharacter, Time.time + delayBetweenAttacks);        //set timer
+                    return true;
                 }
             }
+
+            return false;
         }
 
-        void OnCollisionStay2D(Collision2D collision)
+        bool CheckCollisionStay(Transform collision)
         {
             //do nothing if deactivated (use boolean because the unity toggle doesn't work with collision enter)
             if (isActive == false)
-                return;
+                return false;
 
             //check if hit character and is in the list
             hitCharacter = collision.transform.GetComponentInParent<Character>();
@@ -65,28 +124,32 @@ namespace redd096.GameTopDown2D
                 //damage after delay
                 if (Time.time > hits[hitCharacter])
                 {
-                    OnHit(collision, hitCharacter);
-                    hits[hitCharacter] = Time.time + delayBetweenAttacks;           //reset timer
+                    return true;
                 }
             }
+
+            return false;
         }
 
-        void OnCollisionExit2D(Collision2D collision)
+        bool CheckCollisionExit(Transform collision)
         {
             //do nothing if deactivated (use boolean because the unity toggle doesn't work with collision enter)
             if (isActive == false)
-                return;
+                return false;
 
             //check if exit hit character and is in the list
             hitCharacter = collision.transform.GetComponentInParent<Character>();
             if (hitCharacter && hits.ContainsKey(hitCharacter))
             {
-                //remove from the list
-                hits.Remove(hitCharacter);
+                return true;
             }
+
+            return false;
         }
 
-        void OnHit(Collision2D collision, Character hit)
+        #endregion
+
+        void OnHit(Character hit, Vector2 hitPoint)
         {
             //if can't do damage, return, but continue to check if enter or exit collision
             if (canDoDamage == false)
@@ -107,10 +170,10 @@ namespace redd096.GameTopDown2D
 
             //do damage and push back
             if (hit.GetSavedComponent<HealthComponent>())
-                hit.GetSavedComponent<HealthComponent>().GetDamage(damage, selfCharacter, collision.GetContact(0).point);
+                hit.GetSavedComponent<HealthComponent>().GetDamage(damage, selfCharacter, hitPoint);
 
             if (hit && hit.GetSavedComponent<MovementComponent>())
-                hit.GetSavedComponent<MovementComponent>().PushInDirection(((Vector2)hit.transform.position - collision.GetContact(0).point).normalized, pushForce);
+                hit.GetSavedComponent<MovementComponent>().PushInDirection(((Vector2)hit.transform.position - hitPoint).normalized, pushForce);
         }
 
         /// <summary>
