dae247a1a82ca8b29b3576286bffb834f3c53191
Perdons22 <davide.perdon22@gmail.com>
2022-06-05 15:02:20 +0200

    b-two-with-timer.

---
+++ Assets/Commons/Prefabs/Levels/LevelsScripts/NewLevels/SERVER/B2_Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B2_Timer : MonoBehaviour
+{
+    public float time;
+    public GameObject entrance, exit;
+
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= 1 * Time.deltaTime;
+
+        if(timer <= 0)
+        {
+            entrance.SetActive(false);
+            exit.SetActive(true);
+        }
+    }
+}
